8c71400531b2ea04210de2bbb3bced9f1e79d933
lucianoadorni <lucianoadorni@hotmail.com>
2023-11-26 19:39:05 +0100

    Added Chart.js

--- js/script.js
+++ js/script.js
@@ -1,12 +1,34 @@
 $(document).ready(function(){
-    $('.imageshowcase').slick({
-        dots: true,
-        centerMode: true,
-        arrows: false,
-        infinite: true,
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 3000,
-      });
+  // SLICK
+  $('.imageshowcase').slick({
+      dots: true,
+      centerMode: true,
+      arrows: false,
+      infinite: true,
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 3000,
+    });
+});
+
+// CHART.JS
+const ctx = document.getElementById('myChart');
+new Chart(ctx, {
+  type: 'bar',
+  data: {
+    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+    datasets: [{
+      label: '# of Votes',
+      data: [12, 19, 3, 5, 2, 3],
+      borderWidth: 1
+    }]
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true
+      }
+    }
+  }
 });
